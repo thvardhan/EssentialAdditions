6fadff9c529ebd0832b668173341b824b28c8914
spikespaz <spikespaz@outlook.com>
2017-02-13 10:44:57 -0700

    Converted to ES6

--- assets/js/statistics.js
+++ assets/js/statistics.js
@@ -1,32 +1,30 @@
 update();
 
-setInterval(function() { update(); }, 5000);
+setInterval(() => { update(); }, 5000);
 
 function update() {
-    $.getJSON("https://api.github.com/repos/spikespaz/EssentialAdditions", function(response) {
+    $.getJSON("https://api.github.com/repos/spikespaz/EssentialAdditions", (response) => {
         $('#fork-count').html(response.forks);
     });
 
-    $.getJSON("https://api.github.com/repos/spikespaz/EssentialAdditions/contributors", function(response) {
+    $.getJSON("https://api.github.com/repos/spikespaz/EssentialAdditions/contributors", (response) => {
         $('#contributor-count').html(response.length);
-        var commit_count = 0;
-        for (var i = 0; i < response.length; i++) {
-            commit_count += response[i].contributions;
-        }
+        const commit_count = response.reduce((count, item) => count += item.contributions, 0)
 
         $('#commit-count').html(commit_count);
     });
 
-    $.getJSON("https://api.github.com/repos/spikespaz/EssentialAdditions/releases", function(response) {
-        var filteredReleases = response.filter(function(obj) {
+    $.getJSON("https://api.github.com/repos/spikespaz/EssentialAdditions/releases", (response) => {
+        const filteredReleases = response.filter(function(obj) {
             return (obj.prerelease === false);
         });
 
         if (!$.isEmptyObject(filteredReleases)) {
-            var lastRelease = filteredReleases[0];
-            var version_title = lastRelease.name.split("-");
+            const lastRelease = filteredReleases[0];
+            const version_title = lastRelease.name.split("-");
 
             $('#latest-release').html(version_title[0]);
+            $('#release-count').html(response.length);
             $('#minecraft-version').html(version_title[1]);
             $('#download-release').attr("href", lastRelease.assets[0].browser_download_url);
         }
