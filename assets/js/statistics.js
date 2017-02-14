c38cd2a56f1cab1824937943cfde979d2c82e2af
spikespaz <spikespaz@outlook.com>
2017-02-13 18:06:30 -0700

    Fixed asset location

--- assets/js/statistics.js
+++ assets/js/statistics.js
@@ -1,6 +1,6 @@
 update();
 
-setInterval(() => { update(); }, 5000);
+setInterval(() => { update(); }, 2000);
 
 function update() {
     $.getJSON("https://api.github.com/repos/spikespaz/EssentialAdditions", (response) => {
