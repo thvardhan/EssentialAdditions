8682731b50fad88ddb23d648c9ba1111ff631332
spikespaz <spikespaz@outlook.com>
2017-02-24 06:16:40 -0700

    Changed to using Curse Widget JSON

--- assets/js/statistics.js
+++ assets/js/statistics.js
@@ -1,31 +1,31 @@
-const base_url = "https://api.github.com/repos/spikespaz/EssentialAdditions";
+const github_url = "https://api.github.com/repos/spikespaz/EssentialAdditions";
+const curse_url = "https://widget.mcf.li/mc-mods/minecraft/259003-essential-additions.json";
+const files_base = "https://minecraft.curseforge.com/projects/essential-additions/files/";
 
 update();
 
 setInterval(update(), 60000);
 
 function update() {
-    $.getJSON(base_url, (response) => {
+    $.getJSON(github_url, (response) => {
         $('#fork-count').html(response.forks);
     });
 
-    $.getJSON(base_url + "/contributors", (response) => {
+    $.getJSON(github_url + "/contributors", (response) => {
         $('#contributor-count').html(response.length);
         const commit_count = response.reduce((count, item) => count += item.contributions, 0);
 
         $('#commit-count').html(commit_count);
     });
 
-    $.getJSON(base_url + "/releases/latest", (response) => {
-        const version_title = response.name.split("-");
+    $.getJSON(curse_url, (response) => {
+        const version_number = response.download.name.split(/[\s-]+/g)[1];
+        const download_direct = files_base + response.download.id + "/download";
 
-        $('#latest-release').html(version_title[0]);
-        $('#minecraft-version').html(version_title[1]);
-        $('#download-count').html(response.assets[0].download_count);
-        $('#download-release').attr("href", response.assets[0].browser_download_url);
-    });
-
-    $.getJSON(base_url + "/releases", (response) => {
-        $('#release-count').html(response.length);
+        $('#latest-release').html(version_number);
+        $('#minecraft-version').html(response.download.version);
+        $('#download-count').html(response.downloads.total);
+        $('#download-release').attr("href", download_direct);
+        $('#release-count').html(Object.keys(response.files).length);
     });
 }
