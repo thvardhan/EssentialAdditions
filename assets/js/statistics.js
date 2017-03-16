d77bf72e425ba86bfcac373eca9a8c48e06b5b84
spikespaz <spikespaz@outlook.com>
2017-03-15 22:17:41 -0700

    Added table sorting

--- assets/js/statistics.js
+++ assets/js/statistics.js
@@ -2,9 +2,12 @@ const github_url = "https://api.github.com/repos/spikespaz/EssentialAdditions";
 const curse_url = "https://widget.mcf.li/mc-mods/minecraft/259003-essential-additions.json";
 const files_base = "https://minecraft.curseforge.com/projects/essential-additions/files/";
 
-update();
+$(document).ready(function () {
+    update($('#release-table'));
+});
 
-function update() {
+
+function update(table) {
     $.getJSON(github_url, (response) => {
         $('#fork-count').html(response.forks);
     });
@@ -20,31 +23,41 @@ function update() {
         const version_number = response.download.name.split(/[\s-]+/g)[1];
         const download_direct = files_base + response.download.id + "/download";
 
-        let table_data;
-
-        for (let key in response.files) {
-            let file = response.files[key];
-            let split_name = file.name.split(/[\s-]+/g);
-
-            let date_created = new Date(file.created_at);
-            date_created = date_created.toDateString();
-            table_data = "<tr>" +
-                "\n    <td>" + split_name[0] + "</td>" +
-                "\n    <td>" + split_name[1] + "</td>" +
-                "\n    <td>" + split_name[2] + "</td>" +
-                "\n    <td>" + date_created + "</td>" +
-                "\n    <td>" + file.downloads + "</td>" +
-                "\n    <td>" +
-                "<a class=\"icon fa-download\" href=\"" + files_base + file.id + "/download\"> #" + file.id + "</a></td>" +
-                "</tr>\n" + table_data;
-
-            $('#release-table').html(table_data)
-        }
-
         $('#latest-release').html(version_number);
         $('#minecraft-version').html(response.download.version);
         $('#download-count').html(response.downloads.total);
         $('#download-release').attr("href", download_direct);
         $('#release-count').html(Object.keys(response.files).length);
+
+        if (table) {
+            releasesTable(response);
+            table.tablesorter({
+                sortList: [[4,0]]
+            });
+        }
     });
+}
+
+
+function releasesTable(response) {
+    let table_data;
+
+    for (let key in response.files) {
+        let file = response.files[key];
+        let split_name = file.name.split(/[\s-]+/g);
+
+        let date_created = new Date(file.created_at);
+        date_created = date_created.toDateString();
+        table_data = "<tr>" +
+            "\n    <td>" + split_name[0] + "</td>" +
+            "\n    <td>" + split_name[1] + "</td>" +
+            "\n    <td>" + split_name[2] + "</td>" +
+            "\n    <td>" + date_created + "</td>" +
+            "\n    <td>" + file.downloads + "</td>" +
+            "\n    <td>" +
+            "<a class=\"icon fa-download\" href=\"" + files_base + file.id + "/download\"> #" + file.id + "</a></td>" +
+            "</tr>\n" + table_data;
+
+        $('#release-content').html(table_data)
+    }
 }
